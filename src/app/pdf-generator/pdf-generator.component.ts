4499f62a025d3f8ff1bc1f82e129815d95b295e5
Hulya Karakaya <hulyakarakayaa06@gmail.com>
2023-11-28 09:54:54 -0500

    first commit

---
+++ src/app/pdf-generator/pdf-generator.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import jsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable';
+
+@Component({
+  selector: 'app-pdf-generator',
+  templateUrl: './pdf-generator.component.html',
+  styleUrls: ['./pdf-generator.component.css']
+})
+export class PdfGeneratorComponent {
+
+  generatePDF() {
+    // Create a new PDF document
+    const doc = new jsPDF();
+
+    // Add content to the PDF
+    doc.setFontSize(16);
+    doc.text('My Angular PDF Generator', 10, 10);
+    doc.setFontSize(12);
+    doc.text('This is a comprehensive guide on generating PDFs with Angular.', 10, 20);
+
+    // Create a table using jspdf-autotable
+    const headers = [['Name', 'Email', 'Country']];
+    const data = [
+      ['David', 'david@example.com', 'Sweden'],
+      ['Castille', 'castille@example.com', 'Spain'],
+      // ...
+    ];
+
+    autoTable(doc, {
+      head: headers,
+      body: data,
+      startY: 30 // Adjust the startY position as needed
+    });
+
+    // Save the PDF
+    doc.save('table.pdf');
+  }
+}
